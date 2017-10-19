e7d1a3639f59aff842853096ead1dc7d892f49bd
Jake Carnevale <jake@autocustoms.com>
2017-10-18 21:00:23 -0500

    Begin starter theme

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-// JS will go here
+const navButtons = document.querySelectorAll('.navTrigger')
+const websiteBody = document.querySelector('body')
+const websiteMain = document.querySelector('main')
+// const websiteClose = document.querySelector('main:before')
+
+const menuToggle = (e) => {
+  console.log(e)
+  websiteBody.classList.toggle('is-active')
+}
+
+navButtons.forEach((button) => {
+  button.addEventListener('click', menuToggle)
+})
+
+// if (websiteClose) {
+//   websiteClose.addEventListener('click', menuToggle)
+// }
